660f261f585995e7829e986014bcae771c55479c
deskTop <ganeshkandi024@gmail.com>
2025-04-03 18:17:36 +0530

    new api-3 is introduced to create guest object

--- SunShine/src/main/java/com/project/SunShine/contolller/GuestController.java
+++ SunShine/src/main/java/com/project/SunShine/contolller/GuestController.java
@@ -1,6 +1,7 @@
 package com.project.SunShine.contolller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,44 +15,56 @@ import com.project.SunShine.service.RoomsService;
 @RestController
 public class GuestController {
 
-	@Autowired
-	GuestService guestService;
-
- 
-	  @GetMapping("/getGuestList") public List<Guest> getallStudents()
-	  {
-		  List<Guest>
-	  getallGuests=guestService.getGuestList();
-	  System.out.println("get guest {} "+ getallGuests);
-	  
-	  return getallGuests; 
-	  }
-	 
-
-	@GetMapping(value = "/getguest", produces = "application/json")
-	public Guest getGuest(@RequestParam Integer id) {
-
-		  return guestService.getGuest(id);
-	}
-	
-	@PostMapping("/createguest")
-	public String postMethodName(@RequestBody Guest guest) {
-		return guestService.saveGuest(guest);
-
-	}
-
-	@GetMapping("/deleteguest/{id}")
-	public String deleteGuestInfo(@PathVariable("id") Integer id){
-		  boolean flag = guestService.removeGuest(id);
-		  if(flag){
-			  return "guest deleted successfully";
-		  }else{
-			  return "guest deletion failed";
-		  }
-	}
-	
+    @Autowired
+    GuestService guestService;
 
 
+    @GetMapping("/getGuestList")
+    public List<Guest> getallStudents() {
+        List<Guest>
+                getallGuests = guestService.getGuestList();
+        System.out.println("get guest {} " + getallGuests);
+        return getallGuests;
+    }
+
+
+    @GetMapping(value = "/getguest", produces = "application/json")
+    public Guest getGuest(@RequestParam Integer id) {
+
+        return guestService.getGuest(id);
+    }
+
+    @PostMapping("/createguest")
+    public String createGuest(@RequestBody Guest guest) {
+        return guestService.saveGuest(guest);
+
+    }
+
+    @GetMapping("/deleteguest/{id}")
+    public String deleteGuestInfo(@PathVariable("id") Integer id) {
+        boolean flag = guestService.removeGuest(id);
+        if (flag) {
+            return "guest deleted successfully";
+        } else {
+            return "guest deletion failed";
+        }
+    }
+
+
+    @PostMapping("/createguestlist")
+    public String saveGuestList(@RequestBody  List<Guest> guestList){
+        return guestService.saveGuestList(guestList);
+    }
+
+    @PatchMapping("updatefields/{id}")
+    public Guest updateGuest(@PathVariable int id, @RequestBody Map<String, Object> fields){
+        return guestService.updateGuest(id, fields);
+    }
+
+    @PatchMapping("/updateroom/{id}")
+    public String updateRooms(@PathVariable int id, @RequestBody Map<String, Object> roomDetails){
+        return guestService.updateRooms(id,roomDetails);
+    }
 
 
 }
--- SunShine/src/main/java/com/project/SunShine/service/GuestService.java
+++ SunShine/src/main/java/com/project/SunShine/service/GuestService.java
@@ -1,17 +1,20 @@
 package com.project.SunShine.service;
 
-import java.util.List;
-import java.util.Random;
-
-
+import com.project.SunShine.dao.GuestDao;
 import com.project.SunShine.dao.RoomsDao;
+import com.project.SunShine.model.Guest;
 import com.project.SunShine.model.Rooms;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.project.SunShine.dao.GuestDao;
-import com.project.SunShine.model.Guest;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class GuestService {
@@ -20,6 +23,8 @@ public class GuestService {
     GuestDao guestDao;
     @Autowired
     RoomsDao roomsDao;
+
+    public static final String ROOM_NUMBER = "roomnumber";
 
     public Guest getGuest(Integer guest) {
         System.out.println("we are in service layer..");
@@ -35,7 +40,6 @@ public class GuestService {
     }
 
     public List<Guest> getGuestList() {
-
         return guestDao.findAll();
     }
 
@@ -56,23 +60,35 @@ public class GuestService {
         if(!assignRoom.hasAvailableBeds()){
             return "no beds are available in "+ assignRoom.getRoomNumber();
         }
-        guest.setRoomnumber(assignRoom);
-        guestDao.save(guest);
-
-        assignRoom.setOccupiedBeds(assignRoom.getOccupiedBeds()+1);
-        roomsDao.save(assignRoom);
+        this.updateRoom(guest, assignRoom);
         return "guest "  + guest.getGuest_id() +" assigned the roomNumber : "+assignRoom.getRoomNumber();
     }
 
-    public void saveGuestList(List<Guest> guest) {
-        guestDao.saveAll(guest);
+    private void updateRoom(Guest guest, Rooms room){
+        guest.setRoomnumber(room);
+        guestDao.save(guest);
+
+        room.setOccupiedBeds(room.getOccupiedBeds()+1);
+        roomsDao.save(room);
+    }
+
+    public String saveGuestList(List<Guest> guestList) {
+        if(guestList.isEmpty()){
+            return "guest list is empty.";
+        }else {
+            for(Guest guest: guestList){
+                System.out.println(this.saveGuest(guest));
+            }
+            return "success";
+        }
+
     }
 
     public boolean removeGuest(Integer id) {
         Guest r_guest = guestDao.findById(id).orElseThrow((() -> new RuntimeException("guest not found in DB")));
         guestDao.deleteGuest(r_guest.getGuest_id());
         int roomNumber= r_guest.getRoomnumber().getRoomNumber();
-        int count = incrementRoomNumber(roomNumber);
+        int count = decrementRoomNumber(roomNumber);
         if(count>0){
             System.out.println("room details are also updated successfully");
             return true;
@@ -82,7 +98,55 @@ public class GuestService {
         }
     }
     @Transactional
+    public int decrementRoomNumber(int roomNumber){
+        return roomsDao.updateRoomCount(roomNumber);
+    }
+
+    public Guest  updateGuest(int id, Map<String, Object> fields) {
+        Optional<Guest> u_guest = guestDao.findById(id);
+        if(u_guest.isPresent()) {
+            fields.forEach((key, value) -> {
+                Field field = ReflectionUtils.findField(Guest.class, key);
+                field.setAccessible(true);
+                ReflectionUtils.setField(field, u_guest.get(), value);
+            });
+            return guestDao.save(u_guest.get());
+        }
+        return null;
+    }
+
+    public String updateRooms(int id, Map<String, Object> roomDetails) {
+        Optional<Guest> guest = guestDao.findById(id);
+
+        if(guest.isEmpty() || roomDetails.get(ROOM_NUMBER)== null){
+            return "guest or room not found.. ";
+        }else{
+            int oldRoomNumber = guest.get().getRoomnumber().getRoomNumber();
+            Map<String,Object> objectMap = (Map<String, Object>) roomDetails.get(ROOM_NUMBER);
+
+            int newRoomNumber = (Integer)objectMap.get("roomNumber");
+            this.decrementRoomNumber(oldRoomNumber);
+            Optional<Rooms> newRoom = roomsDao.findById(newRoomNumber);
+            //room number should be updated
+            //and also occupied beds are also update
+            /*roomDetails.forEach((key, value)->{
+                    Field field = ReflectionUtils.findField(Guest.class, key);
+                    field.setAccessible(true);
+                    ReflectionUtils.setField(field, guest.get(),value);
+            });*/
+            if(newRoom.isPresent()) {
+                guest.get().setRoomnumber(newRoom.get());
+                //guestDao.save(guest.get());
+                this.updateRoom(guest.get(), newRoom.get());
+            }
+            return "rooms details are updated for guest : "+ guest.get().getGuest_id() + " and new roomNumber is : "+newRoom.get().getRoomNumber();
+        }
+
+    }
+
+    @Transactional
     public int incrementRoomNumber(int roomNumber){
         return roomsDao.updateRoomCount(roomNumber);
     }
+
 }
